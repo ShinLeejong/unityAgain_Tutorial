f8edbb740657aefcc87b9c9fe87da7921db99bfb
Lee <znlspf123@naver.com>
2022-05-04 18:47:31 +0900

    Third Tutorial - Swipe Car

--- unityTutorialThird_SwipeCar/Third_SwipeCar/Assets/carController.cs
+++ unityTutorialThird_SwipeCar/Third_SwipeCar/Assets/carController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class carController : MonoBehaviour
 {
+    public bool isMoving;
+    public float speed;
+    public AudioSource audio;
+    public Vector2 mouseDownPos;
+    public Vector2 mouseUpPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.speed = 0f;
+        this.audio = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.mouseDownPos = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            if (this.audio.isPlaying) return;
+
+            this.mouseUpPos = Input.mousePosition;
+            try
+            {
+                float gap = this.mouseUpPos.x - this.mouseDownPos.x;
+                this.speed = gap / 500 * 0.7f;
+            } catch (System.Exception e)
+            {
+                Debug.Log(e);
+            } finally
+            {
+                if(!this.audio.isPlaying) 
+                    this.audio.Play();
+            }
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= .88f;
     }
 }
---
+++ unityTutorialThird_SwipeCar/Third_SwipeCar/Assets/director.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class director : MonoBehaviour
+{
+    protected GameObject Car;
+    protected GameObject Flag;
+    protected GameObject Distance;
+
+    public float gap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Car = GameObject.Find("car");
+        this.Flag = GameObject.Find("flag");
+        this.Distance = GameObject.Find("Distance");
+        this.gap = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_pos__car = this.Car.transform.position.x;
+        float x_pos__flag = this.Flag.transform.position.x;
+        this.gap = x_pos__flag - x_pos__car;
+        this.Distance.GetComponent<Text>().text = $"목적지 까지의 거리: {this.gap.ToString("F2")}m";
+    }
+}

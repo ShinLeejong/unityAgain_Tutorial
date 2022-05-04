357c484f58f910c6ef6445198ae30c9e0bdf6bd4
Lee <znlspf123@naver.com>
2022-05-04 11:55:21 +0900

    second tutorial - roulette

--- unityTutorialSecond_Roulette/Second_Roulette/Assets/rouletteController.cs
+++ unityTutorialSecond_Roulette/Second_Roulette/Assets/rouletteController.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class rouletteController : MonoBehaviour
 {
-    public float rotateSpeed;
+    public bool IsRotating;
+    public float RotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        // initialize fields
+        this.RotateSpeed = 0;
+        this.IsRotating = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            this.rotateSpeed = 10f;
+        // on mouse clicked - left click button up
+        if (Input.GetMouseButtonUp(0))
+        {
+            // if roulette is rotating
+            if (this.IsRotating)
+            {
+                this.RotateSpeed = 0;
+                this.IsRotating = false;
+                return; // no need to flow to the last logics
+            } else // if roulette is not rotating(stopped position)
+            {
+                this.RotateSpeed = 20f;
+                this.IsRotating = true;
+            }
+        }
 
-
-        this.rotateSpeed -= 0.3f;
-        transform.Rotate(0, 0, this.rotateSpeed);
+        // Is Roulette ACTUALLY rotating?
+        if (this.RotateSpeed >= 0) 
+        {
+            // keep rotating, but just let it gradually slow down
+            this.RotateSpeed -= 0.15f;
+            // rotate method here
+            transform.Rotate(0, 0, this.RotateSpeed);
+        } else
+        {
+            // NOT ACTUALLY rotating, so store the state as false
+            this.IsRotating = false;
+        }
     }
 }
